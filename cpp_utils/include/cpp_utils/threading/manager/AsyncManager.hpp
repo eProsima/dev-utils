f6f7ddf5c893ebf79636b97fcc898a1ce606f075
jparisu <javierparis@eprosima.com>
2022-11-23 13:21:52 +0100

    Implementation of new Thread Pool
    Signed-off-by: jparisu <javierparis@eprosima.com>

---
+++ cpp_utils/include/cpp_utils/threading/manager/AsyncManager.hpp
@@ -0,0 +1,66 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file AsyncManager.hpp
+ *
+ * This file contains class AsyncManager definition.
+ */
+
+#pragma once
+
+#include <map>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+#include <cpp_utils/library/library_dll.h>
+#include <cpp_utils/threading/manager/IManager.hpp>
+#include <cpp_utils/threading/thread/CustomThread.hpp>
+#include <cpp_utils/types/Atomicable.hpp>
+
+namespace eprosima {
+namespace utils {
+namespace threading {
+
+using TasksCollectionType =
+    Atomicable<
+        std::vector<
+            std::pair<
+                std::unique_ptr<CustomThread>,
+                std::unique_ptr<ITask>>>>;
+
+/**
+ * TODO
+ */
+class AsyncManager : public IManager
+{
+public:
+
+    AsyncManager() = default;
+
+    ~AsyncManager();
+
+    virtual void execute(std::unique_ptr<ITask>&& task) override;
+
+    void clean_threads();
+
+protected:
+
+    TasksCollectionType tasks_running_;
+};
+
+} /* namespace threading */
+} /* namespace utils */
+} /* namespace eprosima */
---
+++ cpp_utils/include/cpp_utils/threading/task/ReferenceTask.hpp
@@ -0,0 +1,47 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file ReferenceTask.hpp
+ *
+ * This file contains class Task definition.
+ */
+
+#pragma once
+
+#include <functional>
+
+#include <cpp_utils/threading/task/ITask.hpp>
+
+namespace eprosima {
+namespace utils {
+namespace threading {
+
+class ReferenceTask : public ITask
+{
+public:
+
+    ReferenceTask(const std::function<void()>* callback_ptr);
+
+    virtual ~ReferenceTask() = default;
+
+    virtual void operator()() noexcept override;
+
+    const std::function<void()>* callback_ptr;
+
+};
+
+} /* namespace threading */
+} /* namespace utils */
+} /* namespace eprosima */

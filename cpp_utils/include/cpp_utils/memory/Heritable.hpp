d9a340ba1ae95d41d2083831594f79ccc3cc07be
jparisu <javierparis@eprosima.com>
2023-02-15 10:51:16 +0100

    Add comment to Heritable
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- cpp_utils/include/cpp_utils/memory/Heritable.hpp
+++ cpp_utils/include/cpp_utils/memory/Heritable.hpp
@@ -26,7 +26,16 @@ namespace eprosima {
 namespace utils {
 
 /**
- * TODO
+ * This class represents an object that could be of the class \c T or any other child of it.
+ *
+ * The motivation for this class is that C++ inheritance is not dynamic and is forced to use pointers, and ptr
+ * arithmetics is different than object arithmetics (e.g. operator==).
+ * Thus, this object can be stored in collections or passed as an on place object and keep its class arithmetic.
+ *
+ * @tparam T class of the internal object.
+ *
+ * @note so far is implemented with an internal shared ptr, what makes it vulnerable to changes in different scopes.
+ * However, the only way to keep this internal ptr safe would be forcing \c T to fulfill the clone interface.
  */
 template <class T>
 class Heritable

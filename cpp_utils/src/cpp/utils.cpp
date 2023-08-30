e494562b45dadcb6d2963c9daf840b4890abf990
Juan Lopez Fernandez <juanlopez@eprosima.com>
2023-08-30 12:09:09 +0200

    Uncrustify
    Signed-off-by: Juan Lopez Fernandez <juanlopez@eprosima.com>

--- cpp_utils/src/cpp/utils.cpp
+++ cpp_utils/src/cpp/utils.cpp
@@ -233,40 +233,58 @@ std::vector<std::string> split_string(
 }
 
 // FROM: https://gist.github.com/williamdes/308b95ac9ef1ee89ae0143529c361d37
-std::string base64_encode(const std::string& in)
+std::string base64_encode(
+        const std::string& in)
 {
     std::string out;
 
-    int val=0, valb=-6;
-    for (unsigned char c : in) {
-        val = (val<<8) + c;
+    int val = 0, valb = -6;
+    for (unsigned char c : in)
+    {
+        val = (val << 8) + c;
         valb += 8;
-        while (valb>=0) {
-            out.push_back(base64_alphabet[(val>>valb)&0x3F]);
-            valb-=6;
+        while (valb >= 0)
+        {
+            out.push_back(base64_alphabet[(val >> valb) & 0x3F]);
+            valb -= 6;
         }
     }
-    if (valb>-6) out.push_back(base64_alphabet[((val<<8)>>(valb+8))&0x3F]);
-    while (out.size()%4) out.push_back('=');
+    if (valb > -6)
+    {
+        out.push_back(base64_alphabet[((val << 8) >> (valb + 8)) & 0x3F]);
+    }
+    while (out.size() % 4)
+    {
+        out.push_back('=');
+    }
     return out;
 }
 
 // FROM: https://gist.github.com/williamdes/308b95ac9ef1ee89ae0143529c361d37
-std::string base64_decode(const std::string& in)
+std::string base64_decode(
+        const std::string& in)
 {
     std::string out;
 
-    std::vector<int> T(256,-1);
-    for (int i=0; i<64; i++) T[base64_alphabet[i]] = i;
+    std::vector<int> T(256, -1);
+    for (int i = 0; i < 64; i++)
+    {
+        T[base64_alphabet[i]] = i;
+    }
 
-    int val=0, valb=-8;
-    for (unsigned char c : in) {
-        if (T[c] == -1) break;
-        val = (val<<6) + T[c];
+    int val = 0, valb = -8;
+    for (unsigned char c : in)
+    {
+        if (T[c] == -1)
+        {
+            break;
+        }
+        val = (val << 6) + T[c];
         valb += 6;
-        if (valb>=0) {
-            out.push_back(char((val>>valb)&0xFF));
-            valb-=8;
+        if (valb >= 0)
+        {
+            out.push_back(char((val >> valb) & 0xFF));
+            valb -= 8;
         }
     }
     return out;

ef20c2d3bfdc8bafc481a850a9f6fe7b5c6836fc
Irene Bandera <irenebandera@eprosima.com>
2023-12-05 09:18:00 +0100

    Add missing thread include
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>

--- cpp_utils/test/unittest/thread_pool/slot_thread_pool_test.cpp
+++ cpp_utils/test/unittest/thread_pool/slot_thread_pool_test.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <thread>
+
 #include <cpp_utils/testing/gtest_aux.hpp>
 #include <gtest/gtest.h>
 
